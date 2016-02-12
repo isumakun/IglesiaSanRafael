aa629beb812836905b24500934c70a102bc66cef
Jorge <>
2016-02-11 20:34:32 -0500

    Organizando el crud

--- funciones/Boleta.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include './conexion.php';
-
-mysql_query("UPDATE boletas SET estado = '$_POST[estado]' WHERE numero = $_POST[bol]");
-
-header("Location: ../iframes/boleteria.php?msj=guardado");
